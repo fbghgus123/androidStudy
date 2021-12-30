70567b41e0e692103e812bdb455376715e88ce94
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2021-12-30 21:51:40 +0900

    Change something

--- study/ch11_jetpack/src/main/java/com/example/ch11_jetpack/OneFragment.kt
+++ study/ch11_jetpack/src/main/java/com/example/ch11_jetpack/OneFragment.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -18,50 +19,48 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.ch11_jetpack.databinding.FragmentOneBinding
 import com.example.ch11_jetpack.databinding.ItemRecyclerviewBinding
 
-// 항목 뷰를 가지는 역할
-class MyViewHolder(val binding: ItemRecyclerviewBinding) :
-        RecyclerView.ViewHolder(binding.root)
-// 어댑터
-class MyAdapter(val datas: MutableList<String>) :
-        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun getItemCount(): Int {
+//항목 View를 가지는 역활
+class MyViewHolder(val binding: ItemRecyclerviewBinding): RecyclerView.ViewHolder(binding.root)
+//항목 구성자. Adapter
+class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    //항목 갯수를 판단하기 위해서 자동 호출
+    override fun getItemCount(): Int{
         return datas.size
     }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
-            RecyclerView.ViewHolder =
-                MyViewHolder(ItemRecyclerviewBinding.inflate(LayoutInflater.from(
-                    parent.context), parent, false))
-
+    //항목의 뷰를 가지는 Holder 를 준비하기 위해 자동 호출
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder
+            = MyViewHolder(ItemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    //각 항목을 구성하기 위해서 호출
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val binding = (holder as MyViewHolder).binding
-        // 뷰에 데이터 출력
-        binding.itemData.text = datas[position]
+        val binding=(holder as MyViewHolder).binding
+        //뷰에 데이터 출력
+        binding.itemData.text= datas[position]
     }
-        }
-// 리사이클러 뷰 꾸미기
+}
+//RecyclerView 꾸미기
 class MyDecoration(val context: Context): RecyclerView.ItemDecoration() {
+    //모든 항목이 출력된후 호출
     override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
         super.onDrawOver(c, parent, state)
-        // 뷰 크기 계산
+        //뷰 사이즈 계산
         val width = parent.width
         val height = parent.height
-        // 이미지 크기 계산
-        val dr: Drawable? = ResourcesCompat.getDrawable(context.resources, R.drawable.kbo, null)
+        //이미지 사이즈 계산
+        val dr: Drawable? = ResourcesCompat.getDrawable(context.getResources(), R.drawable.kbo, null)
         val drWidth = dr?.intrinsicWidth
         val drHeight = dr?.intrinsicHeight
-        // 이미지를 출력할 위치 계산
+        //이미지가 그려질 위치 계산
         val left = width / 2 - drWidth?.div(2) as Int
-        val top = height / 2 - drHeight.div(2) as Int
-        // 이미지 출력
+        val top = height / 2 - drHeight?.div(2) as Int
+        //이미지 출력
         c.drawBitmap(
-            BitmapFactory.decodeResource(context.resources, R.drawable.kbo),
+            BitmapFactory.decodeResource(context.getResources(), R.drawable.kbo),
             left.toFloat(),
             top.toFloat(),
             null
         )
     }
-
+    //각 항목을 꾸미기 위해서 호출
     override fun getItemOffsets(
         outRect: Rect,
         view: View,
@@ -71,31 +70,36 @@ class MyDecoration(val context: Context): RecyclerView.ItemDecoration() {
         super.getItemOffsets(outRect, view, parent, state)
         val index = parent.getChildAdapterPosition(view) + 1
 
-        if (index % 3 == 0) outRect.set(10, 10, 10, 60) // == margin
-        else outRect.set(10, 10, 10, 0)
+        if (index % 3 == 0) //left, top, right, bottom
+            outRect.set(10, 10, 10, 60)
+        else
+            outRect.set(10, 10, 10, 0)
 
         view.setBackgroundColor(Color.parseColor("#28A0FF"))
         ViewCompat.setElevation(view, 20.0f)
+
     }
 }
 
 class OneFragment : Fragment() {
-        override fun onCreateView(
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-            val binding = FragmentOneBinding.inflate(inflater, container, false)
-            // 리사이클러 뷰를 위한 가상 데이터 준비
-            val datas = mutableListOf<String>()
-            for(i in 1..9){
-                datas.add("Item $i")
-            }
-            // 리사이클러 뷰에 LayoutManager, Adapter, ItemDecoration 적용
-            val layoutManager = LinearLayoutManager(activity)
-            binding.recyclerView.layoutManager=layoutManager
-            val adapter=MyAdapter(datas)
-            binding.recyclerView.adapter=adapter
-            binding.recyclerView.addItemDecoration(MyDecoration(activity as Context))
-            return binding.root
+        val binding = FragmentOneBinding.inflate(inflater, container, false)
+        //리사이클러뷰를 위한 가상 데이터 준비...........
+        val datas = mutableListOf<String>()
+        for(i in 1..9){
+            datas.add("Item $i")
+        }
+        //리사이클러뷰에 LayoutManager, Adapter, ItemDecoration 적용
+        val layoutManager = LinearLayoutManager(activity)
+        binding.recyclerView.layoutManager=layoutManager
+        val adapter=MyAdapter(datas)
+        binding.recyclerView.adapter=adapter
+        binding.recyclerView.addItemDecoration(MyDecoration(activity as Context))
+
+        return binding.root
     }
 }
+
