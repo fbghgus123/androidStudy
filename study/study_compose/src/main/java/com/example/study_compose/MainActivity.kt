d1bdd56a2e15da9564454dffc8c6925d3e7d0191
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2022-01-09 19:28:58 +0900

    Study compose tutorial
    튜토리얼 기본 실습 완

--- study/study_compose/src/main/java/com/example/study_compose/MainActivity.kt
+++ study/study_compose/src/main/java/com/example/study_compose/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.study_compose
 
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -18,7 +28,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            Conversation(SampleData.conversationSample)
         }
     }
 }
@@ -34,22 +44,69 @@ fun MessageCard(msg: com.example.study_compose.Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
-            Text(text = msg.author)
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor: androidx.compose.ui.graphics.Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+                ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Colleague", "안녕 제트팩 컴포져")
+    )
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    Row(){
+        Text(
+            text = "..."
+        )
+        LazyColumn {
+            items(messages) { message ->
+                MessageCard(message)}
         }
     }
 }
 
 @Preview
 @Composable
-fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Colleague", "안녕 제트팩 컴포져")
-    )
+fun PreviewConversation() {
+    Conversation(SampleData.conversationSample)
 }
